e2ccb59bd25d29a3b7eb58457ee37eb28a76a5fc
gvbv123 <1595234034@qq.com>
2026-02-28 16:16:43 +0800

    Update loss_factory.py

--- src/losses/loss_factory.py
+++ src/losses/loss_factory.py
@@ -2,12 +2,7 @@ from .bce_posweight import BCEWithPosWeight
 from .focal import FocalLoss
 
 def get_loss(cfg, pos_weight=None):
-    """
-    Build the loss function based on the configuration.
-    cfg["LOSS"]["TYPE"]: "bce_posweight" / "focal"
-    cfg["LOSS"]["FOCAL_GAMMA"]
-    cfg["LOSS"]["FOCAL_ALPHA"]
-    """
+
     loss_type = cfg["LOSS"]["TYPE"].lower()
 
     if loss_type == "bce_posweight":
