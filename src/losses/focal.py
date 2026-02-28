311b0ac156e2b685d492bff4bcbfbaed577fc406
gvbv123 <1595234034@qq.com>
2026-02-28 17:18:16 +0800

    Update focal.py

--- src/losses/focal.py
+++ src/losses/focal.py
@@ -3,7 +3,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class FocalLoss(nn.Module):
-    
     def __init__(self, gamma=2.0, alpha=None, reduction="mean"):
         super().__init__()
         self.gamma = gamma
