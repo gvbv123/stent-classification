7c982cdc78f31b0cecd0bfd58b52d02b9909bb55
gvbv123 <1595234034@qq.com>
2026-02-28 17:18:34 +0800

    Update basic_metrics.py

--- src/metrics/basic_metrics.py
+++ src/metrics/basic_metrics.py
@@ -2,7 +2,6 @@ import numpy as np
 from sklearn.metrics import roc_auc_score, average_precision_score, confusion_matrix, accuracy_score, f1_score
 
 def compute_basic_metrics(y_true, y_prob, prefix=""):
-
     y_true = np.array(y_true)
     y_prob = np.array(y_prob)
     y_pred = (y_prob >= 0.5).astype(int)
